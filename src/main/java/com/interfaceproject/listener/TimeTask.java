fcda07ab1b24c96243b609fa8cfba2abaaa2ef1c
rootczy <365450906@qq.com>
2020-05-28 11:35:49 +0800

    1.增加测试报告邮件发送功能 2.自动生成测试报告集成到项目中可以通过项目直接访问

--- src/main/java/com/interfaceproject/listener/TimeTask.java
+++ src/main/java/com/interfaceproject/listener/TimeTask.java
@@ -1,5 +1,7 @@
 package com.interfaceproject.listener;
 
+import com.interfaceproject.utils.SendEmail;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.testng.TestNG;
@@ -11,14 +13,46 @@ import java.util.List;
 //通过配置文件执行定时任务运行testng.xml文件
 @Service
 public class TimeTask {
+    /**
+     * 邮箱服务器
+     */
+    @Value("${spring.mail.host}")
+    private String host;
+    /**
+     * 发邮件的人的账户
+     */
+    @Value("${spring.mail.username}")
+    private String userName;
+    /**
+     * 发邮件的人的密码
+     */
+    @Value("${spring.mail.password}")
+    private String password;
+    /**
+     * 发给谁
+     */
+    @Value("${test.mail.send}")
+    private String toSend;
+    /**
+     * 抄送人
+     */
+    @Value("${test.mail.cc}")
+    private String cc;
 
     @Scheduled(cron = "${autoRefund.job}")
-    public void runTest() {
+    public void runTest() throws Exception {
         System.out.println("----------开始执行测试用例----------");
         TestNG testNG = new TestNG();
         List<String> suites = new ArrayList<String>();
         suites.add("src\\main\\resources\\testNG.xml");
         testNG.setTestSuites(suites);
         testNG.run();
+        //打包成zip包位置
+        String targetPath = "D:\\";
+        //项目静态文件路径
+        SendEmail.compressedFile("F:\\00_3IdeaProjects\\HttpClientUtils\\HttpClient\\src\\main\\resources\\static", targetPath);
+        List<String> fileList = new ArrayList<>();
+        fileList.add("D:\\static.zip");
+        SendEmail.entity(host, userName, password, toSend, cc, "test", "1234123412", fileList);
     }
 }
---
+++ src/main/java/com/interfaceproject/utils/Log4jUtil.java
@@ -0,0 +1,30 @@
+package com.interfaceproject.utils;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ * log4j
+ * @author jw
+ *
+ */
+public class Log4jUtil {
+
+    public static void info(Object str_info) {
+        StackTraceElement stack[] = (new Throwable()).getStackTrace();
+        Logger logger = Logger.getLogger(stack[1].getClassName());
+        logger.log(Log4jUtil.class.getName(), Level.INFO, str_info, null);
+    }
+
+    public static void error(Object str_err) {
+        StackTraceElement stack[] = (new Throwable()).getStackTrace();
+        Logger logger = Logger.getLogger(stack[1].getClassName());
+        logger.log(Log4jUtil.class.getName(), Level.ERROR, str_err, null);
+    }
+
+    public static void debug(Object str_debug) {
+        StackTraceElement stack[] = (new Throwable()).getStackTrace();
+        Logger logger = Logger.getLogger(stack[1].getClassName());
+        logger.log(Log4jUtil.class.getName(), Level.DEBUG, str_debug, null);
+    }
+}
