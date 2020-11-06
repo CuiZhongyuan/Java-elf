cbb1109786f377c9d71049be185370421f794113
rootczy <365450906@qq.com>
2020-11-06 14:48:53 +0800

    单元测试

--- src/test/java/testcase/TestCaseDemo.java
+++ src/test/java/testcase/TestCaseDemo.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TestCaseDemo {
 
@@ -37,4 +40,50 @@ public class TestCaseDemo {
         Map<String,Object> map = (Map<String, Object>) responseEntity.getBody();
         System.out.println(JsonUtils.mapToJson(map));
     }
+
+    @Test
+    public String case4(){
+        String str = "\ncom.javaelf.service.TestDataFactory\n";
+        String[] out = str.split("\n");
+        for (int i=0;i<out.length;i++){
+            return str;
+        }
+        return null;
+    }
+    @Test
+    public void case5(){
+        System.out.println(case4());
+    }
+
+    @Test
+    public void case6(){
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+        String url1 = " http://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=ww27d6f876d80ceec6&corpsecret=elaiRGcWe4JLESzypgnabA3Tvpj8kLiV6zLEqa4Nbw8";
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.set("Content-Type","application/json");
+        Map<String,Object> map = new HashMap<>();
+        map.put("corpid","ww27d6f876d80ceec6");
+        map.put("corpsecret","elaiRGcWe4JLESzypgnabA3Tvpj8kLiV6zLEqa4Nbw8");
+        String json = JsonUtils.obj2json(map);
+//        String responseEntity = RestTemplateUtils.get(url,String.class,json).getBody();
+        String responseEntity = RestTemplateUtils.get(url,String.class,json).getBody();
+        System.out.println(responseEntity);
+    }
+    @Test
+    public void  case7(){
+        //字符串转map类型Map<String, Object>，再转 Map<String, String>，放入 httpHeaders.setAll(mapStr);
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken";
+        String header = "{\"corpid\":\"${corpidTrue}\",\"corpsecret\":\"${corpidFalse}\"}";
+        Map<String, Object> map1 = JsonUtils.json2map(header);
+        Map<String, String> mapStr = new HashMap<>();
+        map1.keySet().stream().peek(mapKey -> {
+            mapStr.put(mapKey, map1.get(mapKey).toString());
+        }).collect(Collectors.toList());
+        System.out.println(mapStr);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAll(mapStr);
+        String responseEntity = RestTemplateUtils.get(url,httpHeaders,String.class).getBody();
+        System.out.println(responseEntity);
+    }
+
 }
